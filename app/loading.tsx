0ca15af95535f57e8ee412777fdee0410a076c62
Fosberg-codex <fosberg1addai@gmail.com>
2024-08-03 23:11:23 +0000

    small fix in loading update

--- app/loading.tsx
+++ app/loading.tsx
@@ -7,7 +7,7 @@ export default function Loading() {
       <div className='p-4 sm:p-6 md:p-8 lg:p-10 flex justify-center items-center w-full h-full sm:m-8 md:m-16 lg:m-24'>
         <div className='flex flex-col justify-center items-center'>
           <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 border-t-4 border-green border-solid rounded-full animate-spin"></div>
-          <p className="mt-4 text-sm sm:text-base md:text-lg lg:text-xl">Loading...</p>
+          
         </div>
       </div>
     </div>
